40ecbad11ef3ce57c7292ae396c51a956816d566
Muhammad Saim <Saim_DXB@outlook.com>
2023-10-31 22:23:54 +0400

    created lessons.js

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessonsData = [
+    {
+        id: 1001,
+        subject: "Math",
+        location: "London",
+        price: "£100",
+        space: 5,
+        image: "calculator.png",
+        addedToCart: false
+    },
+    {
+        id: 1002,
+        subject: "English",
+        location: "Manchester",
+        price: "£90",
+        space: 5,
+        image: "book.png",
+        addedToCart: false
+    },
+    {
+        id: 1003,
+        subject: "Chemistry",
+        location: "Birmingham",
+        price: "£110",
+        space: 5,
+        image: "flask.png",
+        addedToCart: false
+    },
+    {
+        id: 1004,
+        subject: "Physics",
+        location: "Leeds",
+        price: "£105",
+        space: 5,
+        image: "atom.png",
+        addedToCart: false
+    },
+    {
+        id :1005,
+        subject:"Biology",
+        location:"Glasgow",
+        price:"£95",
+        space :5,
+        image:"dna.png",
+        addedToCart:false
+    },
+    {
+        id :1006,
+        subject:"Computer Science",
+        location:"Sheffield",
+        price:"£120",
+        space :5,
+        image:"computer.png",
+        addedToCart:false
+    },
+    {
+        id :1007,
+        subject:"Geography",
+        location:"Bradford",
+        price:"£85",
+        space :5,
+        image:"globe.png",
+        addedToCart:false
+    },
+    {
+        id :1008,
+        subject:"History",
+        location:"Liverpool",
+        price:"£80",
+        space :5,
+        image:"scroll.png",
+        addedToCart:false
+    },
+    {
+        id :1009,
+        subject:"Art",
+        location:"Edinburgh",
+        price:"£75",
+        space :5,
+        image:"palette.png",
+        addedToCart:false
+    },
+    {
+        id :1010,
+        subject:"Music",
+        location:"Bristol",
+        price:"£70",
+        space :5,
+        image:"music-note.png",
+        addedToCart:false
+    }
+];
